142c0e22eb1396e2e8d27ca7858a3fc0bbe48e98
Matthew Bays <mebays@bsu.edu>
2015-11-19 22:42:21 -0500

    Wrote top ten csv

--- topTen.py
+++ topTen.py
@@ -1,3 +1,5 @@
+#!/bin/bash python
+
 import pandas as pd
 
 
@@ -6,8 +8,10 @@ def main():
     csvPanda = pd.read_csv(
         "/home/mebays/Documents/cs321/proj3/csvFiles/csvData2.csv")
     result = csvPanda.sort(['Number of times'], ascending=[False])
-    topTen = result[['Number of times', 'URL']][0:10]
+    topTen = result[['Number of times', 'URL', 'title', 'text']][0:10]
     topTen.to_csv(
-        "/home/mebays/Documents/cs321/proj3/csvFiles/topTen.csv", sep=',')
+        "/home/mebays/Documents/cs321/proj3/csvFiles/topTen.csv", sep=':')
 
-main()
+if __name__ == '__main__':
+
+    main()
